2eb198930c243481f025730b6dfa895ee4a4e172
ekta2407 <ektathakkar.2407@gmail.com>
2026-06-08 15:35:36 +0530

    Use same-origin API in production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const defaultApiUrl =
-  typeof window === "undefined"
-    ? "http://localhost:5000/api"
-    : `${window.location.protocol}//${window.location.hostname}:5000/api`;
+const defaultApiUrl = import.meta.env.PROD ? "/api" : "http://localhost:5000/api";
 
 export default axios.create({
   baseURL: import.meta.env.VITE_API_URL || defaultApiUrl
